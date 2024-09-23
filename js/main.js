e133728c9c2581d8805154d48049d2359a91e514
AsifurRahman10 <1720105@iub.edu.bd>
2024-09-23 19:48:45 +0600

    toggle feature added

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ document.getElementById('card-1-btn').addEventListener('click', function(){
     // data read
     const card1DonationAmount = inputToValue('card-1-input');
     const card1TotalDonation = elementToNumber('card-1-total-donation');
+    const card1title = elementToText('card-1-title');
     const totalBalance = elementToNumber('total-balance');
     // display the value to card specific amount
     document.getElementById('card-1-total-donation').innerText = card1TotalDonation + card1DonationAmount;
     // minus from main balance
     document.getElementById('total-balance').innerText = totalBalance - card1DonationAmount;
+
+    // update it on history
+    updateOnHistory(card1DonationAmount, card1title);
 })
 
 // card 2 button clicked
@@ -15,11 +19,15 @@ document.getElementById('card-2-btn').addEventListener('click', function(){
     // data read
     const card2DonationAmount = inputToValue('card-2-input');
     const card2TotalDonation = elementToNumber('card-2-total-donation');
+    const card2title = elementToText('card-2-title');
     const totalBalance = elementToNumber('total-balance');
     // display the value to card specific amount
     document.getElementById('card-2-total-donation').innerText = card2TotalDonation + card2DonationAmount;
     // minus from main balance
     document.getElementById('total-balance').innerText = totalBalance - card2DonationAmount;
+
+    // update it on history
+    updateOnHistory(card2DonationAmount, card2title);
 })
 
 // card 3 button clicked
@@ -27,9 +35,27 @@ document.getElementById('card-3-btn').addEventListener('click', function(){
     // data read
     const card3DonationAmount = inputToValue('card-3-input');
     const card3TotalDonation = elementToNumber('card-3-total-donation');
+    const card3title = elementToText('card-3-title');
     const totalBalance = elementToNumber('total-balance');
     // display the value to card specific amount
     document.getElementById('card-3-total-donation').innerText = card3TotalDonation + card3DonationAmount;
     // minus from main balance
     document.getElementById('total-balance').innerText = totalBalance - card3DonationAmount;
+
+    // update it on history
+    updateOnHistory(card3DonationAmount, card3title);
+})
+
+// history btn clicked
+document.getElementById('history-btn').addEventListener('click', function(){
+    document.getElementById('card-container').classList.add('hidden');
+    document.getElementById('history-container').classList.remove('hidden');
+    btnColorShift('history-btn', 'donation-btn');
+})
+
+// donation btn clicked
+document.getElementById('donation-btn').addEventListener('click', function(){
+    document.getElementById('history-container').classList.add('hidden');
+    document.getElementById('card-container').classList.remove('hidden');
+    btnColorShift('donation-btn', 'history-btn');
 })
--- js/utility.js
+++ js/utility.js
@@ -2,7 +2,31 @@
 function inputToValue(id){
     return Number(document.getElementById(id).value);
 }
-// get value form element
+// get number form element
 function elementToNumber(id){
     return Number(document.getElementById(id).innerText);
+}
+function elementToText(id){
+    return document.getElementById(id).innerText;
+}
+// update to history
+function updateOnHistory(donationAmount, cardTitle){
+    const div = document.createElement('div');
+    div.classList.add('border-2', 'rounded-2xl', 'space-y-5', 'p-8')
+    div.innerHTML = `
+    <p class="text-xl font-bold">${donationAmount} Taka is ${cardTitle}</p>
+    <p class="font-light">Date : ${new Date()}</p>
+    `
+    const historyContainer = document.getElementById('history-container');
+    document.getElementById('history-container').insertBefore(div, historyContainer.lastChild)
+}
+
+// btn color shift
+function btnColorShift(clickedID, changedID){
+    // btn color change
+    document.getElementById(changedID).classList.remove('bg-btn_primary');
+    document.getElementById(changedID).classList.add('text-text_light', 'bg-transparent');
+
+    document.getElementById(clickedID).classList.remove('bg-transparent');
+    document.getElementById(clickedID).classList.add('bg-btn_primary'); 
 }
